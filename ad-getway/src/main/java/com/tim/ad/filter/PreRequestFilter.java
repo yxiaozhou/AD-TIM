cf809a7b88d0363396fb19a4a742efc05576e4c6
yxz <459019529@qq.com>
2019-02-27 23:00:26 +0800

    first commit

---
+++ ad-getway/src/main/java/com/tim/ad/filter/PreRequestFilter.java
@@ -0,0 +1,37 @@
+package com.tim.ad.filter;
+
+import com.netflix.zuul.ZuulFilter;
+import com.netflix.zuul.context.RequestContext;
+import com.netflix.zuul.exception.ZuulException;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.cloud.netflix.zuul.filters.support.FilterConstants;
+import org.springframework.stereotype.Component;
+import sun.awt.geom.AreaOp;
+
+@Slf4j
+@Component
+public class PreRequestFilter extends ZuulFilter {
+
+
+    @Override
+    public String filterType() {
+        return FilterConstants.PRE_TYPE;
+    }
+
+    @Override
+    public int filterOrder() {
+        return 0;
+    }
+
+    @Override
+    public boolean shouldFilter() {
+        return true;
+    }
+
+    @Override
+    public Object run() throws ZuulException {
+        RequestContext ctx = RequestContext.getCurrentContext();
+        ctx.set("startTime",System.currentTimeMillis());
+        return null;
+    }
+}
